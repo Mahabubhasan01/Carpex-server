4fbafe6d3aaef90e34811a5f3e7a1e4f955bc3b6
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-26 11:37:31 +0600

    jwt verify token fuction done

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3kfh0.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
 // Json web token -------------------------------------------
 const verifyJWT =(req,res,next)=>{
     const header = req.headers.authorization
@@ -38,26 +29,12 @@ const verifyJWT =(req,res,next)=>{
 
 
 
-async function run(){
-    try{
-        await client.connect();
-        const partsCollection = client.db('carpaex').collection('parts');
-        const productsCollection = client.db('carpaex').collection('products')
 
-        // only six parts collection 
-
-        app.get('/parts',async(req,res)=>{
-            const result = await partsCollection.find({}).toArray();
-            res.send(result)
-        });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3kfh0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-        
-    }
-    finally{
 
-    }
-}
 
 run().catch(console.dir);
 app.listen(port,()=>console.log('Car parts exchange running at',port))
